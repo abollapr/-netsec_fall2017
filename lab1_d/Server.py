f49b4ba791424a6e85d4d0ff02451f9bd49a87da
abollapr <31644822+abollapr@users.noreply.github.com>
2017-09-15 11:54:38 -0400

    Update Server.py

--- lab1_d/Server.py
+++ lab1_d/Server.py
@@ -59,8 +59,6 @@ class  ServerStream(PacketType):
 class ServerProtocol(asyncio.Protocol):
 
     def __init__(self):
-        #self._ip_address = '127.0.0.1'
-        #self._port = 8000
         self.transport = None
         self.GenreList = ["ROCK", "POP", "CLASSICAL", "JAZZ"]
 
